f360d0f4c4f3e8355a95282b082742a35e7c049d
Kendall <ken.nicley@outlook.com>
2021-05-28 18:49:45 -0400

    Added Overlay to Carosuel and updated configs

--- Portfolio/src/app/overlay-carousel/overlay-carousel.component.ts
+++ Portfolio/src/app/overlay-carousel/overlay-carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-overlay-carousel',
@@ -8,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class OverlayCarouselComponent implements OnInit {
 
   images = ['Dark_Road.webp', 'highway.webp', 'TrainTracks.webp'].map((n) => `../assets/img/large/${n}`);
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 10000;
+    config.showNavigationArrows = false;
+   }
 
   ngOnInit(): void {
   }
